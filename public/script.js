7807442d190cb7f3a3c885bad7addff6dc41dbf4
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-25 15:36:41 +0200

    pics

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 let bookLink = "https://striveschool-api.herokuapp.com/books";
+let main = document.querySelector("main");
 
 const loadBooks = function () {
   fetch(bookLink)
@@ -11,6 +12,18 @@ const loadBooks = function () {
     })
     .then((obj) => {
       console.log(obj);
+
+      for (let i = 0; i < obj.length; i++) {
+        let cardContainer = document.createElement("div");
+        cardContainer.className = "flex flex-col items-center justify-between border shadow-md p-2 min-h-[300px]";
+        cardContainer.innerHTML = `<img src="${obj[i].img}" alt="booking" class="h-48 w-full object-cover"/>
+        <h2 class="text-sm font-bold mt-2">${obj[i].title}</h2>
+        <div class="flex mt-2 justify-between w-full">
+       <h3 class="text-slate-400">${obj[i].price} $</h3>
+       <button class="bg-sky-400 text-white p-2 rounded-md hover:bg-orange-500 active:bg-blue-600">Buy Now</button>
+        </div>`;
+        main.appendChild(cardContainer);
+      }
     })
     .catch(
       (err) =>

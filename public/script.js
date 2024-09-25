00c803938d05cb2c950f3bbc4bfcd6a6173f00f0
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-25 15:46:03 +0200

    yes

--- public/script.js
+++ public/script.js
@@ -15,8 +15,8 @@ const loadBooks = function () {
 
       for (let i = 0; i < obj.length; i++) {
         let cardContainer = document.createElement("div");
-        cardContainer.className = "flex flex-col items-center justify-between border shadow-md p-2 min-h-[300px]";
-        cardContainer.innerHTML = `<img src="${obj[i].img}" alt="booking" class="h-48 w-full object-cover"/>
+        cardContainer.className = "flex flex-col items-center justify-between border shadow-md p-2";
+        cardContainer.innerHTML = `<img src="${obj[i].img}" alt="booking" class="h-full w-full"/>
         <h2 class="text-sm font-bold mt-2">${obj[i].title}</h2>
         <div class="flex mt-2 justify-between w-full">
        <h3 class="text-slate-400">${obj[i].price} $</h3>
